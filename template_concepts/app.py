9335d00be5fbcfecda7c4d3048c864bfada8acde
xsetra <abdullahcaliskan511@gmail.com>
2017-07-23 20:46:06 +0300

    Rendering and error handling

--- template_concepts/app.py
+++ template_concepts/app.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from flask.ext.script import Manager
+from flask_script import Manager
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    print(app.url_map)
     return render_template('index.html')
 
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     manager.run()
