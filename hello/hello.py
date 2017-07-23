cf286824fc2bfd2bceb4b694093b231a1b7845a6
xsetra <abdullahcaliskan511@gmail.com>
2017-07-23 13:44:27 +0300

    PEP8

--- hello/hello.py
+++ hello/hello.py
@@ -6,6 +6,7 @@ from flask import Flask
 # Create Flask application object. That requires an arg for resolve the path.
 app = Flask(__name__)
 
+
 # Create a view function for root path. Decorate a python function with app.route()
 @app.route('/')
 def index():
@@ -13,13 +14,14 @@ def index():
     return "<h2>Hello, FLASK!</h2>", 200
     # 200 is a status_code of response. It's not require
 
+
 @app.route('/user/<name>')
 def get_user(name):
     return "Welcome, {}".format(name), 200
 
+
 def list_users():
     return "LIST OF USERS", 200
-
 app.add_url_rule(rule='/user', endpoint='list_users', view_func=list_users)
 
 # Run Flask web server. Debug mode on, port 8000.
