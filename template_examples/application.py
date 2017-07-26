5c7358a63e35aafc6c0bdc50988a281a9d8e4914
xsetra <abdullahcaliskan511@gmail.com>
2017-07-26 23:26:38 +0300

    Header html created and other templates extends. Contents added.

--- template_examples/application.py
+++ template_examples/application.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('main.html')
+
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=1)
