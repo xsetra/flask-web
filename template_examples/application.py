8be0b583d0fa33eebfdccc5b920343f342a7a0c9
xsetra <abdullahcaliskan511@gmail.com>
2017-07-27 00:32:16 +0300

    Flask flash function implementation and secret key set.

--- template_examples/application.py
+++ template_examples/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 
 app = Flask(__name__)
 
@@ -10,6 +10,9 @@ def home():
 
 @app.route('/dashboard/')
 def dashboard():
+    flash('TEST 1')
+    flash('TEST MESSAGE 2')
+    flash('TEST MESSAGE EXAMPLES 3')
     return render_template('dashboard.html', TOP_CONTENT=content())
 
 
@@ -33,4 +36,5 @@ def content():
     return CONTENT
 
 if __name__ == '__main__':
+    app.secret_key = "secret_key_first"
     app.run(debug=1)
