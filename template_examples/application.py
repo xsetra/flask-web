21eac0d94ef80e5d0b188f3e7bddc607d10097ed
xsetra <abdullahcaliskan511@gmail.com>
2017-07-30 14:40:09 +0300

    Basic login page and control

--- template_examples/application.py
+++ template_examples/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -26,7 +26,24 @@ def slashboard():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
-    return render_template('login.html')
+    error = ''
+    try:
+        if request.method == "POST":
+
+            attempted_username = request.form['username']
+            attempted_password = request.form['password']
+
+            if attempted_username == "admin" and attempted_password == "password":
+                return redirect(url_for('dashboard'))
+
+            else:
+                error = "Invalid credentials. Try Again."
+
+        return render_template("login.html", error=error)
+
+    except Exception as e:
+        # flash(e)
+        return render_template("login.html", error=error)
 
 
 @app.errorhandler(404)
